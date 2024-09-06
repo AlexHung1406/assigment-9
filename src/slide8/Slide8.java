fcb3ae092ba3d5f5cb2bcf303bfff923bfb372b5
AlexHung1406 <truongvanhung14062005@gmail.com>
2024-09-07 01:20:59 +0700

    Update Slide8.java

--- src/slide8/Slide8.java
+++ src/slide8/Slide8.java
@@ -10,11 +10,54 @@ package slide8;
  */
 public class Slide8 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
+//    public class Overloading {
+////    public static double sum(int a, int b){
+////        return a+b;
+////    } error code
+//    public static int sum(int a, int b,int c){
+//        return a+b+c;
+//    }
+//    public static int sum(int a, int b){
+//        return a+b;
+//    }
+//}
+    
+    
+    
+//        //    public static double sum(int a, int b){
+////        return a+b;
+////    } error code
+//    public static int sum(int a, int b,int c){
+//        return a+b+c;
+//    }
+//    public static int sum(int a, int b){
+//        return a+b;
+//    }
+    
+     
+    
+    
+//    public class Student {
+//
+//    int ID;
+//    String name;
+//
+//    public Student(int ID, String name) {
+//        this.ID = ID;
+//        this.name = name;
+//    }
+//
+//    void displayInformation() {
+//        System.out.println("ID: " + ID + ", Name: " + name);
+//    }
+//
+//    public static void main(String[] args) {
+//        Student student1 = new Student(1, "Nam");
+//        Student student2 = new Student(2, "Lan");
+//        student1.displayInformation();
+//        student2.displayInformation();
+//    }
+   
     
 }
